423319f937cbb1da6d96610ff75da0d1b0aa5a8b
timbetters <65300955+timbetters@users.noreply.github.com>
2020-05-14 15:39:56 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,12 +5,12 @@ node 'master.puppet.vm' {
   file { '/root/README':
   ensure => file,
   content => $fqdn,
+ }
 }
-
 node /^web/ { 
   include role::app_server
  }
-}
+ 
 node /^db/ {
   include role::db_server
 }
