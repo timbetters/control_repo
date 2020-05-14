688e32ef3d72817aa115673a4ef3834f62c7cbd7
timbetters <65300955+timbetters@users.noreply.github.com>
2020-05-14 15:57:52 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCn8U7PKR4uUW3+Mq2Xi6O7hAFep7G+2iN16MMhIo6KuSQJrTsqijYatr0ppaWbpjBiHzgPJKlwu0BUxG6G+Mmmls1+EPTTzEVwK+TynQUGRpIpex2iS484QnGxqGxUoUBwAjItZ9OzdtNglbrJCjNO+tRhhyuYy/69ZKHalFMzkszI9b30rXnyylvT8ThB39l8TInlcy6Rnb737NI+j5ZaKTYz6Qpmf0nJuJtl4D1Cq1J9DHosNU7j2kYj8pCx24rX0R2Qc//TOnux4NZxfMpX+qFyuuk1OaG3JEBr+s4d3a18A/IrkTPby0J5r6fFOzIKB8056RZQpu0oF3ML3b9l',
 	}  
 }
