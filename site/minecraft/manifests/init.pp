f21705f594f1120d835f45be1097c3570fda399b
timbetters <65300955+timbetters@users.noreply.github.com>
2020-05-15 18:28:40 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,5 @@
 class minecraft {
-  file {‘/opt/minecraft’:
-    ensure => directory,
-  }
+  
   file {‘/opt/minecraft/minecraft_server.jar’:
     ensure => file,
     source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
