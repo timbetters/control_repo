2ec3da5bffd965cefb41e70a6552a5b54a6774d4
timbetters <65300955+timbetters@users.noreply.github.com>
2020-05-15 13:19:42 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,29 +1,29 @@
 class minecraft {
-  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
-  $install_dir = '/opt/minecraft'
-){
-  file { $install_dir:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file { "${install_dir}/minecraft_server.jar":
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => $url,
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+    before => Service[‘minecraft’],
   }
 
-  package {'java':
+
+  package {‘java’:
     ensure => present,
   }
-  file {"${install_dir}/eula.txt":
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true'
+    content => ‘eula=true’
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
   }
-  service { 'minecraft':
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
+    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
   }
 }
 
